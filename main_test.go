fdcb35a583d6d243a84ff1cc63f27749e652ddee
Gahan12 <gahantentwal12@gmail.com>
2023-12-18 01:51:18 +0530

    try

---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main_test
+
+type Tests struct {
+	ID             int    `json:"id"`
+	Owner          string `json:"owner"`
+	Type           string `json:"type"`
+	Color          string `json:"color"`
+	Vehicle_No     string `json:"vehicle_no"`
+	Model          string `json:"model"`
+	Defective_Part string `json:"defective_part"`
+	Amount         string `json:"amount"`
+	Check_in       string `json:"check_in"`
+}
